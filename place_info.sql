fc9927f9c8ea306a5f7473fd1132dce3c66cb98b
Sorawit <2110511106002@live4.utcc.ac.th>
2023-11-21 15:48:00 +0700

    update

--- place_info.sql
+++ place_info.sql
@@ -1,31 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 10, 2023 at 11:14 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `place`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `place_info`
---
 
 CREATE TABLE `place_info` (
   `id` int(10) NOT NULL,
@@ -34,10 +10,6 @@ CREATE TABLE `place_info` (
   `latitude` text NOT NULL,
   `longitude` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `place_info`
---
 
 INSERT INTO `place_info` (`id`, `nameplace`, `detail`, `latitude`, `longitude`) VALUES
 (1, 'Chiang Mai Thailand', 'Many travelers prefer Thailand\'s pleasant northern capital over the hectic pace of life in Bangkok.', '18.796143', '98.979263'),
@@ -51,27 +23,9 @@ INSERT INTO `place_info` (`id`, `nameplace`, `detail`, `latitude`, `longitude`) 
 (9, 'Tokyo Japan', 'Perhaps not the cheapest of places to visit in Asia, bustling Tokyo is the world\'s largest metropolitan economy.', '35.6762', '139.6503'),
 (10, 'Mount Fuji Japan', 'Mount Fuji belongs to the group of volcanic mountains. You don\'t need mountaineering equipment for a walking tour of Mount Fuji.', '35.3606', '138.7274');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `place_info`
---
 ALTER TABLE `place_info`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `place_info`
---
 ALTER TABLE `place_info`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
